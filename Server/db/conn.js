4ad4468840c28633a8da402868b4168f23e2f482
PranshuSon1 <97882904+PranshuSon1@users.noreply.github.com>
2022-04-22 13:46:47 +0530

    initial

---
+++ Server/db/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const DB = "mongodb://localhost/pagination"
+
+
+
+mongoose.connect(DB,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=> console.log("connection start")).catch((error)=> console.log(error.message));
